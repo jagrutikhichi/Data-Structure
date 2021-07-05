3208d21bb6a9554dfdc4362c28f1a6b7975272b5
Jagruti Khichi <49718077+jagrutikhichi@users.noreply.github.com>
2021-07-05 17:01:29 +0530

    Added: comments

--- src/com/bridgelabz/stack/MyStack.java
+++ src/com/bridgelabz/stack/MyStack.java
@@ -2,6 +2,7 @@ package com.bridgelabz.stack;
 
 public class MyStack {
 	MyStackNode head;
+	//Method to add data into stack
 	public void push (int data) {
 		MyStackNode newNode = new MyStackNode();
 		newNode.data = data;
@@ -9,6 +10,7 @@ public class MyStack {
 		head = newNode;
 		
 	}
+	//Method to print Stack
 	public void printStack() {
 		MyStackNode tempNode = head;
 		while(tempNode != null) {
@@ -16,9 +18,11 @@ public class MyStack {
 			tempNode =tempNode.next;
 		}
 	}
+	//Method to show top/peak element in stack
 	public int peek() {
 		return head.data;
 	}
+	//Method to remove data from top (LILO)
 	public int pop() {
 		MyStackNode tempNode = head;
 		head =head.next;
